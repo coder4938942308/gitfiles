511d06e4265847eb51461182d8cd6aefd8dff16f
coder4938942308 <111892207+coder4938942308@users.noreply.github.com>
2022-10-12 10:54:54 +0100

    Add files via upload

---
+++ productQA/Main.java
@@ -0,0 +1,51 @@
+package com.qa.java.Collection;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Main {
+
+	public ArrayList<Product> readProduct(String filePath) {
+		ArrayList<Product> productlist = new ArrayList<>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(new File(filePath)))) {
+			String prodData = br.readLine();
+			prodData = br.readLine();
+			int indexer = 0;
+
+
+			while (prodData != null) {
+
+				String[] prodInfoList = prodData.split(",");
+				String name = prodInfoList[0];
+				float price = Float.parseFloat(prodInfoList[1]);
+				int stock = Integer.parseInt(prodInfoList[2]);
+				boolean availability = Boolean.parseBoolean(prodInfoList[3]);
+
+				Product product = new Product();
+				product.setName(name)
+				.setPrice(price)
+				.setStock(stock)
+				.setAvailable(availability);
+
+				productlist.add(product);
+
+				prodData = br.readLine();
+			}
+
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+		return productlist;
+	}
+}
---
+++ productQA/Product.java
@@ -0,0 +1,87 @@
+package com.qa.java.Collection;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+	
+	private String name;
+	private float price;
+	private int stock;
+	private boolean available;
+	
+	
+//	public Product(String name, float price, int stock, boolean available) {
+//		super();
+//		this.name = name;
+//		this.price = price;
+//		this.stock = stock;
+//		this.available = available;
+//	}
+
+	
+	public Product() {};
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public Product setName(String name) {
+		if (name == null)
+			this.name = "";
+		this.name = name;
+		return this;
+	}
+
+
+	public float getPrice() {
+		return price;
+	}
+
+
+	public Product setPrice(float price) {
+		this.price = price;
+		return this;
+	}
+
+
+	public int getStock() {
+		return stock;
+	}
+
+
+	public Product setStock(int stock) {
+		this.stock = stock;
+		return this;
+	}
+
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+
+	public Product setAvailable(boolean available) {
+		this.available = available;
+		return this;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Product [name=" + name + ", price=" + price + ", stock=" + stock + ", available=" + available + "]";
+	}
+	
+	
+	
+	
+
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Priv
